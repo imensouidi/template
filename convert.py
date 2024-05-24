154888d7885e2856eeb0f65320e8bde416fc1f8e
aous <abelhedi@heptasys.com>
2024-05-24 15:19:54 +0100

    template

--- convert.py
+++ convert.py
@@ -235,8 +235,7 @@ def save_html_to_file(html_content, output_file):
 
 def convert_html_to_pdf(html_file, pdf_file):
     try:
-        options = {
-            'enable-local-file-access': '',  # Enable access to local files
+        options = {           
             'page-size': 'A4',
             'encoding': "UTF-8",
             'no-outline': None,  # No outline around the text
@@ -316,3 +315,4 @@ def upload_template():
 if __name__ == "__main__":
     os.makedirs("uploads", exist_ok=True)
     app.run(host='0.0.0.0', port=5000, debug=True)
+
